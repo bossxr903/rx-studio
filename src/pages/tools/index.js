2a40689a57c2ba214102faa0d5b6e140ba7d8dbc
MD. ARAFAT RAHMAN <bossxr567@gmail.com>
2026-04-01 21:00:44 +0600

    Update index.js

---
+++ src/pages/tools/index.js
@@ -0,0 +1,48 @@
+// Import the template for all tools initially
+import ToolTemplate from './ToolTemplate';
+
+// When you create actual tool files, import them here:
+// import InvoiceForge from './InvoiceForge';
+// import ResumeRocket from './ResumeRocket';
+
+// All tools use the coming soon template by default
+export const InvoiceForge = ToolTemplate;
+export const ResumeRocket = ToolTemplate;
+export const ContractCraft = ToolTemplate;
+export const MeetMate = ToolTemplate;
+export const FocusFlow = ToolTemplate;
+export const LinkVault = ToolTemplate;
+export const NoteNest = ToolTemplate;
+export const StandupBot = ToolTemplate;
+export const BudgetBuddy = ToolTemplate;
+export const LoanLens = ToolTemplate;
+export const CoinFlip = ToolTemplate;
+export const TaxTally = ToolTemplate;
+export const SplitEase = ToolTemplate;
+export const NetWorthNow = ToolTemplate;
+export const FreelanceFee = ToolTemplate;
+export const ExpenseSnap = ToolTemplate;
+export const FlashGenius = ToolTemplate;
+export const QuizForge = ToolTemplate;
+export const GradeGrid = ToolTemplate;
+export const StudySync = ToolTemplate;
+export const CiteCraft = ToolTemplate;
+export const TypingTrail = ToolTemplate;
+export const MindMapMaker = ToolTemplate;
+export const CountdownClass = ToolTemplate;
+export const BodyCheck = ToolTemplate;
+export const MealMap = ToolTemplate;
+export const HydroTrack = ToolTemplate;
+export const FitForge = ToolTemplate;
+export const MedMinder = ToolTemplate;
+export const SleepScore = ToolTemplate;
+export const MoodLog = ToolTemplate;
+export const StepStreak = ToolTemplate;
+export const PalettePickr = ToolTemplate;
+export const GradientGuru = ToolTemplate;
+export const QRQuick = ToolTemplate;
+export const ShadowSmith = ToolTemplate;
+export const FontFusion = ToolTemplate;
+export const IconSnap = ToolTemplate;
+export const ContrastCheck = ToolTemplate;
+export const ScreenSizer = ToolTemplate;
